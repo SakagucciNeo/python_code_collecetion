553083d164ec853e86f869972044343efc10a97b
SakagucciNeo <nicegnice29@gmail.com>
2021-12-27 07:56:26 +0900

    cheched

---
+++ study_folium.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 23 08:35:50 2021
+
+@author: sakaguchino
+"""
+
+import folium
+import pandas as pd
+
+# # このCSVには、県庁所在地の緯度・経度がlatitudeカラムとlongitudeカラムに入っている。
+# df_prefecture = pd.read_csv("../data/prefectural_capital_locations.csv")
+
+df = pd.read_excel('onsen_output01.xlsx')
+
+
+def visualize_locations(df,  zoom=4):
+    """日本を拡大した地図に、pandasデータフレームのlatitudeおよびlongitudeカラムをプロットする。
+    """
+
+    # 図の大きさを指定する。
+    f = folium.Figure(width=1000, height=500)
+
+    # 初期表示の中心の座標を指定して地図を作成する。
+    center_lat = 34.686567
+    center_lon = 135.52000
+    m = folium.Map([center_lat, center_lon], zoom_start=zoom).add_to(f)
+
+    # データフレームの全ての行のマーカーを作成する。
+    for i in range(0, len(df)):
+        folium.Marker(location=[df["緯度"][i],
+                      df["経度"][i]]).add_to(m)
+
+    return m
+
+
+visualize_locations(df)
