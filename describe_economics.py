409dbf7bbcf7e762f7b9da5b5c48a98eb100c83b
SakagucciNeo <nicegnice29@gmail.com>
2021-12-28 07:29:43 +0900

    checked

---
+++ describe_economics.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 28 07:24:37 2021
+
+@author: sakaguchino
+"""
--- study_folium.py
+++ study_folium.py
@@ -1,12 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Dec 23 08:35:50 2021
-
-@author: sakaguchino
-"""
-
 import folium
 import pandas as pd
+
 
 # # このCSVには、県庁所在地の緯度・経度がlatitudeカラムとlongitudeカラムに入っている。
 # df_prefecture = pd.read_csv("../data/prefectural_capital_locations.csv")
@@ -15,7 +9,7 @@ df = pd.read_excel(
     'C:/Users/user/Documents/GitHub/python_code_collecetion/onsen_output01.xlsx')
 
 
-def visualize_locations(df,  zoom=4):
+def visualize_locations(df,  zoom=15, tiles="Stamen Terrain"):
     """日本を拡大した地図に、pandasデータフレームのlatitudeおよびlongitudeカラムをプロットする。
     """
 
@@ -23,8 +17,8 @@ def visualize_locations(df,  zoom=4):
     f = folium.Figure(width=1000, height=500)
 
     # 初期表示の中心の座標を指定して地図を作成する。
-    center_lat = 34.686567
-    center_lon = 135.52000
+    center_lat = df["緯度"][1]
+    center_lon = df["経度"][1]
     m = folium.Map([center_lat, center_lon], zoom_start=zoom).add_to(f)
 
     # データフレームの全ての行のマーカーを作成する。
